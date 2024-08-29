aaa5e763445d0d1e828dbeb8b4ed90a77443ca0d
Georgia Tsirigotaki <gtsirigotaki@byte.gr>
2024-08-29 22:58:30 +0300

    Add project files.

---
+++ StealAllTheCats.Data/Entities/Cat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealAllTheCats.Data.Entities
+{
+    public class Cat
+    {
+        /// <summary>
+        /// ID of the cat
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// ID of the image returned from CaaS API
+        /// </summary>
+        public int CatId { get; set; }
+
+        /// <summary>
+        /// Width of the image returned from CaaS API
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Height of the image returned from CaaS API
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Image of the cat
+        /// </summary>
+        public byte[]? Image { get; set; }
+
+        /// <summary>
+        /// Image filename
+        /// </summary>
+        public string? ImageFilepath { get; set; }
+
+        /// <summary>
+        /// DateTime creation of the cat
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        public virtual ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
+
+    }
+}
---
+++ StealAllTheCats.Data/Entities/Tag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealAllTheCats.Data.Entities
+{
+    /// <summary>
+    /// Class Tag.
+    /// </summary>
+    public class Tag
+    {
+        /// <summary>
+        /// Id of the tag
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the tag
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cat id.
+        /// </summary>
+        public int? CatId { get; set; }
+
+        /// <summary>
+        /// Cat of the candidate
+        /// </summary>
+        public virtual Cat Cat { get; set; }
+
+        /// <summary>
+        /// DateTime creation of the tag
+        /// </summary>
+        public DateTime Created { get; set; }
+
+    }
+}
